6d3a44a7251a57cfad1cfbcc1949dc74c1bf6df4
gntouts <ece8441@upnet.gr>
2022-04-19 16:50:51 +0300

    added go file

---
+++ cmdcap.go
@@ -0,0 +1,61 @@
+package cmdcap
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+)
+
+func catch(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createLogDir(path string) string {
+	_, err := os.Stat(path)
+	if err != nil {
+		err := os.Mkdir(path, 0755)
+		catch(err)
+	}
+	return path
+}
+
+func countFiles(path string) int {
+	files, err := ioutil.ReadDir(path)
+	catch(err)
+	return len(files)
+}
+
+func fname(path string) string {
+	files := countFiles(path)
+	filesn := strconv.Itoa(files)
+	return path + "log-" + filesn + ".log"
+}
+
+func createLogFile(path string) string {
+	filename := fname(path)
+	file, err := os.Create(filename)
+	catch(err)
+	defer file.Close()
+	return file.Name()
+}
+
+func CaptureCmd(path string) {
+	createLogDir(path)
+	logFile := createLogFile(path)
+
+	file, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE, 0666)
+	catch(err)
+	defer file.Close()
+
+	argsWithProg := os.Args
+
+	w := bufio.NewWriter(file)
+	fmt.Fprintln(w, argsWithProg)
+
+	w.Flush()
+}
